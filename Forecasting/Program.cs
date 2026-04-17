1c9d704bcc9c86dc4b55030a0d7bbb174a1d17da
darckronoz <crispipe14@gmail.com>
2026-04-17 10:25:32 -0500

    agregar cors policy para front

--- Forecasting/Program.cs
+++ Forecasting/Program.cs
@@ -12,11 +12,23 @@ var connectionString = builder.Configuration.GetConnectionString("DevConnection"
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddScoped<SalesRepository>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowDevCors",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+app.UseCors("AllowDevCors");
 
 app.UseAuthorization();
 
