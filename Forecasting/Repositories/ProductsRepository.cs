83f80aa0dbd711822f2029bef75564f449ad9012
darckronoz <crispipe14@gmail.com>
2026-04-19 10:38:35 -0500

    create post sales endpoint

---
+++ Forecasting/Repositories/ProductsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Forecasting.Data;
+using Forecasting.Sales.Entity;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forecasting.Repositories
+{
+    public class ProductsRepository(AppDbContext _context)
+    {
+        public Product? GetProductByIdentificator(string identificator)
+        {
+            return _context.Products.FirstOrDefault(p => p.Identificator.Equals(identificator));
+        }
+    }
+}
