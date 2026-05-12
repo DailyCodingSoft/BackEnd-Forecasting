8a33c427aa826d6a03fabe211829065d9c696799
darckronoz <crispipe14@gmail.com>
2026-05-12 09:43:35 -0500

    agregar categoria a producto

--- Forecasting/Utils/ProductMapper.cs
+++ Forecasting/Utils/ProductMapper.cs
@@ -6,11 +6,11 @@ namespace Forecasting.Utils
     {
         public static List<ProductFilterDto> MapProductsToDto(List<Product> products)
         {
-            return products.Select(p => new ProductFilterDto
+            return [.. products.Select(p => new ProductFilterDto
             {
                 Identificator = p.Identificator,
                 ProductName = p.ProductName
-            }).ToList();
+            })];
         }
     }
 }
