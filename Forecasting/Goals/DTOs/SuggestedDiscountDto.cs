0c2e08d34491e66b0597ca0116db4a46ea67d02c
Santiago Rivera <david.rivera04@uptc.edu.co>
2026-05-21 18:41:20 -0500

    Se agrega identificador a precios sugeridos

--- Forecasting/Goals/DTOs/SuggestedDiscountDto.cs
+++ Forecasting/Goals/DTOs/SuggestedDiscountDto.cs
@@ -4,6 +4,7 @@ namespace Forecasting.Goals.DTOs
     {
         public int SuggestedDiscountId { get; set; }
         public required string Identificator { get; set; }
+        
         public int GoalId { get; set; }
         public required string ProductName { get; set; }
         public decimal MinimumPrice { get; set; }
